0d015ad2279e31af4df7fccbd0e2539870699ea0
T- Rex <t-rex@T-s-iMac.bbrouter>
2023-12-31 22:21:33 +0530

    file structure and code optimized

--- automater.py
+++ automater.py
@@ -8,7 +8,7 @@ mainobj = codeprocessor.objcode
 postcode = codeprocessor.postcode
 endCode = codeprocessor.endCode
 
-f= open("main.gcode","w+")
+f= open("gcodes/main.gcode","w+")
 for index in range(repeat):
     f.write(str(precode))
     f.write(str("M290 Z"+babystep +"\n"+mainobj))
